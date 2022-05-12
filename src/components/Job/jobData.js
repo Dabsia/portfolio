47273baef36423b153bb15a03a0e86b5c6eec296
Dabsia <dabojohnson@gmail.com>
2022-05-12 02:58:49 -0700

    Created links for my project images

--- src/components/Job/jobData.js
+++ src/components/Job/jobData.js
@@ -4,14 +4,14 @@ export const jobData = [
     {
         'name' : 'Lunchup',
         'jobDesc' : 'Proxy provider website including authentication, dashboard and dynamic features',
-        'img' : 'https://raw.githubusercontent.com/Dabsia/lunchup/develop/src/assets/giant-burger.png',
+        'img' : 'https://raw.githubusercontent.com/Dabsia/portfolio/master/src/Assets/lunchup.png',
         'language' : 'React - Redux - Firebase',
         'githubLink' : 'https://github.com/Dabsia/lunchup'
     },
     {
         'name' : 'Netflix',
         'jobDesc' : 'A clone of the popular Netflix Application',
-        'img' : 'https://raw.githubusercontent.com/Dabsia/lunchup/develop/src/assets/giant-burger.png',
+        'img' : 'https://raw.githubusercontent.com/Dabsia/portfolio/master/src/Assets/netflix.png',
         'language' : 'React - Redux - TheMovieDb API',
         'githubLink' : 'https://github.com/Dabsia/netflix-clone-master'
     }
